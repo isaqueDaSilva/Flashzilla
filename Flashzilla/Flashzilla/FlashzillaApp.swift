e20f23033f1c9111b8f65b5f6e9dbe6b854f8e23
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-10-13 22:08:01 -0300

    initial commit

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Isaque da Silva on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
